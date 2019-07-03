f0c785f19c8fc8a5b36381365401b4b82f274365
Tino Lange <coldcoff@yahoo.com>
2019-07-04 00:02:18 +0200

    Bugfix: use unicode literals also on python2, this avoids errors with formatstring.format(...) (see: https://stackoverflow.com/questions/3235386/python-using-format-on-a-unicode-escaped-string)

--- gp-okta.py
+++ gp-okta.py
@@ -26,7 +26,7 @@
    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
    THE SOFTWARE.
 """
-from __future__ import print_function
+from __future__ import print_function, unicode_literals
 import io, os, sys, re, json, base64, getpass, subprocess, shlex, signal
 from lxml import etree
 import requests
